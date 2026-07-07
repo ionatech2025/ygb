ed5fe663f7e0906e999eb6c018aba9d983bccadb
Shemuel <2060newty@gmail.com>
2026-07-07 10:56:23 +0300

    scaffolded a bit of the needed setup

---
+++ backend/src/main/java/com/ionatech/ygb/backend/YgbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ionatech.ygb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgbBackendApplication.class, args);
+	}
+
+}
